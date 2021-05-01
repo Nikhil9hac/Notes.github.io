d54567623192e392be539d3831afe682c5d8832c
Nikhil <kumarnikhilsingh.hac07@gmail.com>
2021-05-01 19:59:19 +0530

    search bar

--- js/searcher.js
+++ js/searcher.js
@@ -1,17 +1,52 @@
+
 // targeting the Element
 const searcher=document.getElementById('searcherBar');
 const searcherBtn=document.querySelector('.searcherBtn');
 const card=document.querySelectorAll('.card');
-searcherBtn.addEventListener('click',()=>{
+const hide=document.querySelector('.hide');
+const main=document.querySelector('.cardContainer');
+blockCount=0;
+const cardModifier=()=>{
     card.forEach(element=>{
-        let data=element.getAttribute('data-value')
-        let data2=element.getAttribute('data-value2')
-        let data3=element.getAttribute('data-value3')
-        if (data==searcher.value || data2==searcher.value) {
+        let data=element.getAttribute('data-value');
+        let data2=element.getAttribute('data-value2');
+        let data3=element.getAttribute('data-value3');
+        if (data.toLowerCase()==searcher.value.toLowerCase() ||data2.toLowerCase()==searcher.value.toLowerCase()) {
             element.style.display="block";
             element.style.display="flex";
+            element.removeAttribute('data-aos')
         } else {
-            element.style.display='none'
+            element.style.display='none';
         }
+       
+        
     })
+}
+
+searcherBtn.addEventListener('click',()=>{
+    cardModifier()
 })
+setInterval(()=>{
+    if (searcher.value=="") {
+        card.forEach((element)=>{
+            element.style.display="block";
+            element.style.display="flex";
+            element.setAttribute('data-aos','fade-right');
+        })
+    }
+    card.forEach(element=>{
+        let elemVal=window.getComputedStyle(element);
+    if (elemVal.display=="flex") {
+        blockCount++
+    }
+})
+if (blockCount!=0) {
+    hide.style.display='none';
+    main.style.display='grid';
+    blockCount=0;
+}
+else{
+    hide.style.display='block'
+    main.style.display='block'
+}
+},1000);
